700237a6034ad6f5426c0e93ec96b57e536c4ceb
Damian Tomczak <contact@damian-tomczak.pl>
2024-01-28 00:08:49 +0100

    commit

--- assets/shaders/shader.frag
+++ assets/shaders/shader.frag
@@ -8,5 +8,20 @@ out vec4 outColor;
 
 void main()
 {
-    outColor = texture(texture1, texCoord);
+    vec2 adjustedCoord = texCoord;
+//    float screenAspectRatio = 1;
+//    float textureAspectRatio = 1.227;
+//
+//    if (screenAspectRatio > textureAspectRatio)
+//    {
+//        float scale = screenAspectRatio / textureAspectRatio;
+//        adjustedCoord.y = (texCoord.y - 0.5) * scale + 0.5;
+//    }
+//    else
+//    {
+//        float scale = textureAspectRatio / screenAspectRatio;
+//        adjustedCoord.x = (texCoord.x - 0.5) * scale + 0.5;
+//    }
+
+    outColor = texture(texture1, adjustedCoord);
 }
--- main.cpp
+++ main.cpp
@@ -27,18 +27,14 @@ std::optional<Shader> fullscreenShader;
 class Window
 {
 public:
+    inline static float scaleFactor{};
+
     Window(const std::string& title, int width, int height, Vector2D position) :
         mPosition{position}, mWidth{width}, mHeight{height}, mTitle{title}
     {
         mpWindow = SDL_CreateWindow(mTitle.c_str(), static_cast<int>(mPosition.x), static_cast<int>(mPosition.y), mWidth, mHeight,
             SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL | SDL_WINDOW_BORDERLESS);
         assert(mpWindow);
-
-        SDL_SysWMinfo wmInfo{};
-        SDL_GetWindowWMInfo(mpWindow, &wmInfo);
-
-        SetWindowPos(wmInfo.info.win.window, HWND_TOPMOST, static_cast<int>(mPosition.x), static_cast<int>(mPosition.y),
-            0, 0, SWP_NOMOVE | SWP_NOSIZE);
     }
 
     virtual ~Window()
@@ -49,7 +45,11 @@ public:
 
     void update()
     {
-        SDL_SetWindowPosition(mpWindow, static_cast<int>(mPosition.x), static_cast<int>(mPosition.y));
+        SDL_SysWMinfo wmInfo{};
+        SDL_GetWindowWMInfo(mpWindow, &wmInfo);
+
+        SetWindowPos(wmInfo.info.win.window, HWND_TOPMOST, static_cast<int>(mPosition.x), static_cast<int>(mPosition.y),
+            0, 0, SWP_NOSIZE);
     }
 
 protected:
@@ -73,8 +73,9 @@ class Entity final : public Window
 {
 public:
     inline static float maxSpeed{};
+    inline static float pushbackStrength{};
 
-    Entity(EntityType type, Vector2D position, int width, int height, float speed = vw * 3, const std::string& title = "")
+    Entity(EntityType type, Vector2D position, int width, int height, float speed = vw * 20, const std::string& title = "")
         : Window{title, width, height, position}, mSpeed{speed}
     {
         mpContext = SDL_GL_CreateContext(mpWindow);
@@ -119,12 +120,36 @@ public:
 
     void update(const float deltaTime)
     {
-        applyFriction(static_cast<float>(vw * 5));
+        applyFriction(static_cast<float>(-(vw * 0.05f)));
 
         mVelocity.x = std::max(std::min(mVelocity.x, maxSpeed), -maxSpeed);
         mVelocity.y = std::max(std::min(mVelocity.y, maxSpeed), -maxSpeed);
 
-        mPosition += mVelocity * deltaTime;
+
+        SDL_DisplayMode mode{};
+        SDL_GetDisplayMode(0, 0, &mode);
+
+        const Vector2D newPos = mPosition + mVelocity * deltaTime;
+
+        if (newPos.x < 0)
+        {
+            mVelocity.x = pushbackStrength;
+        }
+        else if (newPos.x + mWidth > mode.w)
+        {
+            mVelocity.x = -pushbackStrength;
+        }
+
+        if (newPos.y < 0)
+        {
+            mVelocity.y = pushbackStrength;
+        }
+        else if (newPos.y + mHeight > mode.h)
+        {
+            mVelocity.y = -pushbackStrength;
+        }
+
+        mPosition = mPosition + mVelocity * deltaTime;
 
         Window::update();
     }
@@ -137,7 +162,23 @@ private:
 
     void applyFriction(float friction)
     {
-        mVelocity *= friction;
+        if (mVelocity.x > 0)
+        {
+            mVelocity.x = std::max(mVelocity.x + friction, 0.f);
+        }
+        else if (mVelocity.x < 0)
+        {
+            mVelocity.x = std::min(mVelocity.x - friction, 0.f);
+        }
+
+        if (mVelocity.y > 0)
+        {
+            mVelocity.y = std::max(mVelocity.y + friction, 0.f);
+        }
+        else if (mVelocity.y < 0)
+        {
+            mVelocity.y = std::min(mVelocity.y - friction, 0.f);
+        }
     }
 };
 
@@ -159,13 +200,16 @@ public:
 
 int main(int argc, char** argv) try
 {
+    SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_SYSTEM_AWARE);
+
     App app;
 
-    SDL_DisplayMode mode;
+    SDL_DisplayMode mode{};
     SDL_GetDisplayMode(0, 0, &mode);
     vw = (mode.w / 100.f);
     vh = (mode.h / 100.f);
     Entity::maxSpeed = vw * 20;
+    Entity::pushbackStrength = vw * 100;
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
@@ -173,7 +217,8 @@ int main(int argc, char** argv) try
 
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
-    Entity player{EntityType::PLAYER, {mode.w / 2.f, mode.h / 2.f}, static_cast<int>(10 * vw), static_cast<int>(10 * vw)};
+    const int playerSize{static_cast<int>(10.f * vw)};
+    Entity player{EntityType::PLAYER, {(mode.w / 2.f) - playerSize / 2.f, (mode.h / 2.f) - playerSize / 2.f}, playerSize, playerSize};
 
     glGenVertexArrays(1, &emptyVAO);
 
